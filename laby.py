592f090062783319c00614b6e0e5029795c3ad40
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-05-21 11:58:15 -0400

    Adiciona background vermelho transparente ao perder

--- laby.py
+++ laby.py
@@ -2,19 +2,20 @@ import pygame
 import random
 import time
 
-# Dimensões da janela do jogo
-WINDOW_WIDTH = 900  # Aumenta a largura da janela
-WINDOW_HEIGHT = 900  # Aumenta a altura da janela
+# Dimensões da janela do Laby
+WINDOW_WIDTH = 900  # Aumenta a largura
+WINDOW_HEIGHT = 900  # Aumenta a altura
 
 # Tamanho dos blocos do labirinto
 BLOCK_SIZE = 20
 
-# Cores
+# Paleta de cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 GOLD = (255, 223, 0)
+TRANSPARENT_RED = (255, 0, 0, 128)
 
 # Inicializa o pygame
 pygame.init()
@@ -23,9 +24,10 @@ pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Laby")
 
+# Controla o framerate do Laby
 clock = pygame.time.Clock()
 
-# Gera um labirinto usando o algoritmo de Prim
+# Gera labirintos usando o algoritmo de Prim
 def generate_maze(width, height):
     maze = [[1] * width for _ in range(height)]
 
@@ -54,6 +56,7 @@ def generate_maze(width, height):
 
     return maze
 
+# Desenha o labirinto na janela
 def draw_maze(maze, width, height):
     for y, row in enumerate(maze):
         for x, cell in enumerate(row):
@@ -65,16 +68,18 @@ def draw_maze(maze, width, height):
             else:
                 pygame.draw.rect(window, WHITE, rect)
 
-    # Desenha um quadrado branco na posição inicial (spawn) do jogador
+    # Desenha um caminho no spawn do jogador
     spawn_x, spawn_y = (width - 45, height - 44)
     start_rect = pygame.Rect((spawn_x + 1) * BLOCK_SIZE, (spawn_y + 1) * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
     pygame.draw.rect(window, WHITE, start_rect)
 
+# Desenha o jogador na janela
 def draw_player(player_pos):
     x, y = player_pos
     rect = pygame.Rect((x + 1) * BLOCK_SIZE, (y + 1) * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
     pygame.draw.rect(window, GREEN, rect)
 
+# Exibe mensagens no meio da tela
 def display_message(msg):
     font = pygame.font.Font(None, 70)
     text = font.render(msg, True, GOLD)
@@ -84,11 +89,20 @@ def display_message(msg):
     window.blit(shadow, shadow_rect)
     window.blit(text, text_rect)
 
+# Fim de jogo
+def game_over():
+    overlay = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
+    overlay.fill(TRANSPARENT_RED)
+    window.blit(overlay, (0, 0))
+    display_message("Tempo esgotado! Você perdeu.")
+
+# Recomeça o jogo
 def reset_game(width, height):
     maze = generate_maze(width, height)
     player_pos = (width - 45, height - 44)  # Posição inicial do jogador
     return maze, player_pos
 
+# Núcleo principal do Laby
 def main():
     width = (WINDOW_WIDTH - BLOCK_SIZE) // BLOCK_SIZE
     height = (WINDOW_HEIGHT - BLOCK_SIZE) // BLOCK_SIZE
@@ -107,6 +121,7 @@ def main():
                 pygame.quit()
                 return
 
+        # Atalhos
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP] or keys[pygame.K_w]:
             new_pos = (player_pos[0], player_pos[1] - 1)
@@ -125,14 +140,16 @@ def main():
             if maze[new_pos[1]][new_pos[0]] in [0, 2]:
                 player_pos = new_pos
 
-        elapsed_time = int(time.time() - start_time) 
+        elapsed_time = int(time.time() - start_time) # Tempo total de jogo
 
-        remaining_time = max(0, countdown_duration - elapsed_time)
+        remaining_time = max(0, countdown_duration - elapsed_time) # Tempo restante
 
+        # Desenhando o labirinto e o jogador...
         window.fill(BLACK)
         draw_maze(maze, width, height)
         draw_player(player_pos)
 
+        # Exibe o tempo restante + efeito pisca-pisca quando faltar 10 segundos
         if remaining_time <= 10:
             blink_timer += clock.get_rawtime()
             if blink_timer >= 50:
@@ -148,16 +165,19 @@ def main():
             font = pygame.font.Font(None, 24)
             time_text = font.render("Tempo restante: {} segundos".format(remaining_time), True, WHITE)
 
+        # Posicionando o tempo restante...
         time_rect = time_text.get_rect(topleft=(3, 3))
         window.blit(time_text, time_rect)
 
+        # Condicional de derrota
         if remaining_time <= 0:
-            display_message("Tempo esgotado! Você perdeu!")
+            game_over()
             pygame.display.flip()
             pygame.time.wait(3000)
             maze, player_pos = reset_game(width, height)
             start_time = time.time()
 
+        # Condicional de vitória
         if maze[player_pos[1]][player_pos[0]] == 2:
             display_message("Você escapou do labirinto!")
             pygame.display.flip()
@@ -165,8 +185,8 @@ def main():
             maze, player_pos = reset_game(width, height)
             start_time = time.time()
 
-        pygame.display.flip()
-        clock.tick(15)
+        pygame.display.flip() # Atualiza janela
+        clock.tick(15) # Limita a 15 fps
 
 if __name__ == '__main__':
     main()
